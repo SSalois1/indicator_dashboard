03e4b03aa052e3718f5846fdcdfb3340261ba7ef
sarah salois <sarah.salois@noaa.gov>
2024-10-11 14:01:27 -0400

    saving just in case

---
+++ app/old_animations_tab.R
@@ -0,0 +1,32 @@
+Animations 
+=====================================  
+  
+  Column {data-width=350}
+-----------------------------------------------------------------------
+  
+  ### Sea Surface Temperature 
+  
+  ```{r, sst animation, eval=FALSE}
+tags$video(
+  src = "www/DD_20230821_20230902-ACSPO-GHRSST_15_30-V2.0-NES-SST_15_30-FPS_3.webm",
+  type = "video/webm",
+  width = 550, height = 700,
+  controls = "controls"
+)
+
+```
+
+
+Column {data-width=350}
+-----------------------------------------------------------------------
+  
+  ### Chlorophyll-A
+  
+  ```{r, chl animation, eval=FALSE}
+tags$video(
+  src = "www/DD_20230821_20230902-GLOBCOLOUR-MULTI-R2019-NES-CHLOR_A_0.1_10-GSM-FPS_3.webm",
+  type = "video/webm",
+  width = 550, height = 700,
+  controls = "controls"
+)
+```
